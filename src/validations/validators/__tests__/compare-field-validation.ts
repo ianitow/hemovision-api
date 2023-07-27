da08c565663066305e1a1aeadc1888990763d698
ianitow <iaan.developer@gmail.com>
2023-07-26 22:24:34 -0300

    test: create compare field validation

---
+++ src/validations/validators/__tests__/compare-field-validation.ts
@@ -0,0 +1,21 @@
+import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
+import { CompareFieldValidation } from '../compare-field-validation'
+
+interface SutTypes {
+  sut: CompareFieldValidation
+}
+const makeSut = (): SutTypes => {
+  return {
+    sut: new CompareFieldValidation('any_field', 'field_to_compare')
+  }
+}
+describe('compare-field-validation', () => {
+  test('Should returns an error if field are different', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({
+      any_field1: 'any_value',
+      field_to_compare: 'different_value'
+    })
+    expect(error).toBe(new InvalidParamError('field_to_compare'))
+  })
+})
