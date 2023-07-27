5c36b2edfc9f58e13e145fd23608f3eabb110c01
ianitow <iaan.developer@gmail.com>
2023-07-26 23:36:15 -0300

    feat: http helpers

---
+++ src/presentation/helpers/http-helpers.ts
@@ -0,0 +1,26 @@
+import { type HttpResponse } from '../protocols/http-response'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new Error(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
