41591cc276ea60ace2701b19071d4eaa73de6742
ianitow <iaan.developer@gmail.com>
2023-07-26 22:08:22 -0300

    feat: ensure required field validation validate field

---
+++ src/validations/validators/required-field-validation.ts
@@ -0,0 +1,12 @@
+import { MissingParamError } from "../../presentation/errors/missing-param-error";
+import { Validation } from "../../presentation/protocols/validation";
+
+export class RequiredFieldValidation implements Validation{
+    constructor(private readonly fieldName: string){}
+    validate (input: any): Error | null{
+        if(!input[this.fieldName]){
+            return new MissingParamError(this.fieldName)
+        }
+        return null
+    }
+}
