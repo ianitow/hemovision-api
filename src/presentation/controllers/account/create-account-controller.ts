47684facce5df94df518deab8b8ddff3dadc2862
ianitow <iaan.developer@gmail.com>
2023-07-26 21:49:57 -0300

    feat: should throw error if validation fails

--- src/presentation/controllers/account/create-account-controller.ts
+++ src/presentation/controllers/account/create-account-controller.ts
@@ -1,11 +1,14 @@
+import { MissingParamError } from '../../errors/missing-param-error'
 import { type Controller } from '../../protocols/controller'
 import { type HttpResponse } from '../../protocols/http-response'
 
 export class CreateAccountController implements Controller {
   async handle (request: any): Promise<HttpResponse> {
+    if(!request.email) throw new MissingParamError('email')
+    if(!request.name) throw new MissingParamError('name')
     return {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body: new MissingParamError('name')
 
     }
   }
