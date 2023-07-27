9bd501624d2d6ab6d0cd573da01ff1d6ea77399d
ianitow <iaan.developer@gmail.com>
2023-07-26 22:23:12 -0300

    feat: create required field validation

--- src/validations/validators/required-field-validation.ts
+++ src/validations/validators/required-field-validation.ts
@@ -1,12 +1,12 @@
-import { MissingParamError } from "../../presentation/errors/missing-param-error";
-import { Validation } from "../../presentation/protocols/validation";
+import { MissingParamError } from '../../presentation/errors/missing-param-error'
+import { type Validation } from '../../presentation/protocols/validation'
 
-export class RequiredFieldValidation implements Validation{
-    constructor(private readonly fieldName: string){}
-    validate (input: any): Error | null{
-        if(!input[this.fieldName]){
-            return new MissingParamError(this.fieldName)
-        }
-        return null
+export class RequiredFieldValidation implements Validation {
+  constructor (private readonly fieldName: string) {}
+  validate (input: any): Error | null {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName)
     }
+    return null
+  }
 }
